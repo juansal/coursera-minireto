cc8676eb19bc4737c73d71835bef4c8862c8a023
SaveR <savermsx@gmail.com>
2016-02-06 13:52:22 +0100

    Minireto

---
+++ semana2_minireto/minireto.playground/Contents.swift
@@ -0,0 +1,22 @@
+// Semana2:
+
+import UIKit
+
+for k in 0...100 {
+
+    if k >= 30 && k <= 40 {
+        print("# \(k) Viva Swift")
+    }
+    if k%5 == 0 {
+        print("# \(k) Bingo!!!")
+    }
+    if k%2 == 0 {
+        print("# \(k) Par")
+    }
+    if k%2 != 0 {
+        print("# \(k) Impar")
+    }
+
+}
+
+
